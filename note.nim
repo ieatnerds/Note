951ff113db5094c0c8feaa60ac57fd30cce65f8b
Nick Kelly <importantnk@gmail.com>
2017-10-28 12:27:53 -0400

    Added comment. Updated custom exception.

--- note.nim
+++ note.nim
@@ -7,25 +7,28 @@ var message: string
 message = ""
 
 type
-    mycustomerror* = object of Exception
+    argError* = object of Exception
 
 proc head(): void =
+    # append date to head of message
     message = getDateStr()
     message.add("-->")
     message.add(getClockStr())
     message.add("\n")
 
 proc isarg(arg: string): bool =
+    # Returns true if passed string starts with "-"
     if arg.startsWith("-"):
         return true
     else:
         return false
 
 proc executearg(arg: string): void =
+    # executes given argument
     if(toLowerAscii(arg) == "-t"):  # t for Timex
         head()
     else:
-        raise newException(mycustomerror,"Argument not recognized.")
+        raise newException(argError,"Argument not recognized.")
             
 var
     o = open("notes.txt", fmAppend)
@@ -34,7 +37,7 @@ let arguments = commandLineParams()
 for argument in arguments:
     if(isarg(argument)):
         executearg(argument)
-        # message = message.add(argument&" ")
+
     else:
         message.add(argument)
         message.add(" ")
