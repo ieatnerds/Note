ef207477d15d01a81006b1cc0fc8d0f29738385b
Nick Kelly <importantnk@gmail.com>
2017-11-02 22:09:53 -0400

    Changed spacing for indentation

--- note.nim
+++ note.nim
@@ -12,87 +12,88 @@
 ###         ###
 
 import strutils, osproc, os, times, terminal, sequtils
+### Variables###
 var
-    file: File
-    message: string
-message = ""
+  file: File
+  message: string = ""
 
+###Types###
 type
-    argError* = object of Exception
+  argError* = object of Exception
 
 ###       ###
 ### Flags ###
 ###       ###
 
 proc head(): void =
-    # Append date to head of message
-    message = getDateStr()
-    message.add("-->")
-    message.add(getClockStr())
-    message.add("\n")
+  # Append date to head of message
+  message = getDateStr()
+  message.add("-->")
+  message.add(getClockStr())
+  message.add("\n")
 
 proc help(): void =
-    # This flag will display help options to the console when used.
-    setForegroundColor(fgBlue)
-    stdout.write "Help! \n"
-    writeStyled "This is the help menu for note.nim \n"
-    stdout.write "More coming soon!\n"
-    stdout.write "The current arguments for this program include\n"
-    writeStyled "1. -h For the help menu\n"
-    writeStyled "2. -t To append time to the top of the note\n"
-    writeStyled "3. -f For specifying a note txt file.\n"
-    resetAttributes()
-    stdout.write "\n"
-    quit()
+  # This flag will display help options to the console when used.
+  setForegroundColor(fgBlue)
+  stdout.write "Help! \n"
+  writeStyled "This is the help menu for note.nim \n"
+  stdout.write "More coming soon!\n"
+  stdout.write "The current arguments for this program include\n"
+  writeStyled "1. -h For the help menu\n"
+  writeStyled "2. -t To append time to the top of the note\n"
+  writeStyled "3. -f For specifying a note txt file.\n"
+  resetAttributes()
+  stdout.write "\n"
+  quit()
     
 ###            ###
 ### Procedures ###
 ###            ###
  
 proc isArg(arg: string): bool =
-    # Returns true if passed string starts with "-"
-    if arg.startsWith("-"):
-        return true
-    else:
-        return false
+  # Returns true if passed string starts with "-"
+  if arg.startsWith("-"):
+    return true
+  else:
+    return false
 
 proc noteFile(filename = "notes.txt"): File =
-    # Used to specify the file to save notes into
-    result = open(filename, fmAppend)
+  # Used to specify the file to save notes into
+  result = open(filename, fmAppend)
     
 proc writeMess(message: string): void =
-    # This procedure will write a give message to the open file 
-    # and then it will close the file, assuming that the file should not 
-    # be kept open when we are no longer using it.
-    file.writeLine(message)
-    file.close()
+  # This procedure will write a give message to the open file 
+  # and then it will close the file, assuming that the file should not 
+  # be kept open when we are no longer using it.
+  file.writeLine(message)
+  file.close()
 
 proc main(): void =
-    # Main will do the heavy lifting of the program, as usual, tying everything
-    # else in the program together.
-    var
-        arguments = commandLineParams()
-        k = 0
-    file = noteFile()
-    for i in 0..(high(arguments)):
-        if(isArg(arguments[i])):
-            if(arguments[i] == "-h"): # help
-                help()
-            if(arguments[i] == "-f"): # file
-                k = i+1
-                file = noteFile(arguments[k])
-                arguments[k] = ""
-                # arguments.delete(k)
-                # arguments.add("")
-            if(arguments[i] == "-t"): # time append
-                head()
-        else:
-            message.add(arguments[i])
-            message.add(" ")
+  # Main will do the heavy lifting of the program, as usual, tying everything
+  # else in the program together.
+  var
+    arguments = commandLineParams()
+    k = 0
+  file = noteFile()
+  for i in 0..(high(arguments)):
+    if(isArg(arguments[i])):
+      if(arguments[i] == "-h"): # help
+        help()
+      if(arguments[i] == "-f"): # file
+        k = i+1
+        file = noteFile(arguments[k])
+        arguments[k] = ""
+        # arguments.delete(k)
+        # arguments.add("")
+      if(arguments[i] == "-t"): # time append
+        head()
+    else:
+      message.add(arguments[i])
+      message.add(" ")
             
-    message.add("\n")
-    message.add("----------------------------")
-    writeMess(message)
+  message.add("\n")
+  message.add("----------------------------")
+  writeMess(message)
 
 ###      ###
 ### Main ###
